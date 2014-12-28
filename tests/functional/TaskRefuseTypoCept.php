4857a7a1909ebbd21c73ef9c73c536c8ff39b313
Benoit Boure <benoit.boure@bbwebconsult.com>
2014-12-28 20:32:10 +0100

    Correct mistake in refusal with typo test

--- tests/functional/TaskRefuseTypoCept.php
+++ tests/functional/TaskRefuseTypoCept.php
@@ -3,7 +3,7 @@
 use Vreasy\Models\Task;
 
 $I = new TestGuy($scenario);
-$I->wantTo('Check refusal of a task');
+$I->wantTo('Check refusal of a task with typo');
 $I->haveTask(['assigned_name' => 'Benoit', 'assigned_phone' => '+34444444444', 'state'=> Task::STATE_PENDING]);
-$I->sendPOST('/twilio/update?format=json', ['From' => '+34444444444', 'Body' => 'NO']);
+$I->sendPOST('/twilio/update?format=json', ['From' => '+34444444444', 'Body' => 'NOP']);
 $I->seeInDatabase('tasks', ['assigned_phone' => '+34444444444', 'state' => Task::STATE_REFUSED]);
